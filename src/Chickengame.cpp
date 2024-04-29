0f620863b38d42123847d64842ec01cf36fbb427
Nimac0 <iamnimaco@gmail.com>
2024-04-30 01:21:46 +0200

    ref(Powerups): move powerup adding functionality to gamedev side

--- src/Chickengame.cpp
+++ src/Chickengame.cpp
@@ -1,8 +1,11 @@
-#include "Game.h"
+#include "Chickengame.h"
 
-#include "../engine/include/Game.h"
-#include "AssetManager.h"
-#include "HealthComponent.h"
+#include "Pickupables.h"
+
+#include <iostream>
+#include <Game.h>
+#include <AssetManager.h>
+#include <HealthComponent.h>
 
 void chickengame::init()
 {
@@ -15,9 +18,13 @@ void chickengame::update()
 
 	if (powerupSpawn == 0)
 	{
+		int pickupableType = rand() % chickengame::pickupables::pickupableList.size();
+		std::tuple pickupable = chickengame::pickupables::pickupableList.at(pickupableType);
+		// gen rand tuple
 		engine::game->assets->createPowerup(
 			engine::game->assets->calculateSpawnPosition(),
-			engine::game->assets->calculateType()
+			std::get<0>(pickupable), // tuple[0]
+			std::get<1>(pickupable) // tuple[1]
 		);
 	}
 
---
+++ src/Pickupables.cpp
@@ -0,0 +1,20 @@
+#include "Pickupables.h"
+
+#include <HealthComponent.h>
+#include <StatEffectsComponent.h>
+
+void chickengame::pickupables::heartEffect(Entity* player)
+{
+    if(player->getComponent<HealthComponent>().getHealth() < 5)
+        player->getComponent<HealthComponent>().modifyHealth(1);
+}
+
+void chickengame::pickupables::movementSpeedEffect(Entity* player)
+{
+    player->getComponent<StatEffectsComponent>().modifyStatDur(Stats::MOVEMENT_SPEED, BUFF_DURATION);
+}
+
+void chickengame::pickupables::atkSpeedEffect(Entity* player)
+{
+    player->getComponent<StatEffectsComponent>().modifyStatDur(Stats::ATTACK_SPEED, BUFF_DURATION);
+}
